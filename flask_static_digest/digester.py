b67bdb0a890b191f26e40dff7006bcb5e7d2b93f
Whiskey <scott@deltapaintball.com>
2020-01-22 15:37:31 -0500

    Add support for windows
    Add support for windows

--- flask_static_digest/digester.py
+++ flask_static_digest/digester.py
@@ -101,8 +101,7 @@ def _generate_manifest(files, gzip_files, output_path):
     manifest = {}
 
     for file in files:
-        raw_rel_file_path = os.path.relpath(file, output_path)
-        rel_file_path = "/".join(raw_rel_file_path.split("\\"))
+        raw_rel_file_path = os.path.relpath(file, output_path).replace("\\", "/")
 
         file_name, file_extension = os.path.splitext(rel_file_path)
 
