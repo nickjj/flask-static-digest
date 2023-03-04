e03ef72dbe357a1fcfefd7959845948a652b0658
maxdup <mdupuis@hotmail.ca>
2023-03-03 20:05:54 -0500

    fleshed out example app with multiple blueprints

--- tests/example_app/example/app.py
+++ tests/example_app/example/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_static_digest import FlaskStaticDigest
 
+from .page1.bp_page1 import bp_page1
+from .page2.bp_page2 import bp_page2
+
 flask_static_digest = FlaskStaticDigest()
 
 
@@ -8,6 +11,9 @@ def create_app():
     app = Flask(__name__)
 
     app.config.from_object("config.settings")
+
+    app.register_blueprint(bp_page1)
+    app.register_blueprint(bp_page2)
 
     flask_static_digest.init_app(app)
 
